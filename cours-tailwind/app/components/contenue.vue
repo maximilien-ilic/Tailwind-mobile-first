79e88441f0df42b69b2ee59bbedbf530c3b82eee
max <maximilien.ilic@gmail.com>
2025-10-12 22:59:59 +0200

    derniers ajustements

--- cours-tailwind/app/components/contenue.vue
+++ cours-tailwind/app/components/contenue.vue
@@ -4,47 +4,47 @@ import { Ellipsis } from 'lucide-vue-next';
 </script>
 
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
-    <div class="bg-white border border-gris-foncer p-4 rounded-xl w-full h-auto min-h-56">
-        <p class="  font-medium">Create catalog</p>
-        <p class="text-text pb-6 pt-5  font-normal">Create new catalog to add products and generate content.</p>
-        <bouton class="flex justify-center mt-7 py-3 w-full">
-        <PlusSquare /> Create catalog
-        </bouton>
-    </div>
-    <div class="bg-white border border-gris-foncer p-4 rounded-xl w-full h-auto min-h-56">
-        <div class="flex justify-between items-start">
-        <p class="pt-7 pb-8  text-2xl font-semibold">Christmas 2024</p>
-        <div class="mt-5 opacity-60">
-            <Ellipsis/>
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4 mt-7">
+        <div class="bg-white border border-gris-foncer p-4 rounded-xl w-full h-auto min-h-56">
+            <p class="  font-medium">Create catalog</p>
+            <p class="text-text pb-6 pt-5  font-normal">Create new catalog to add products and generate content.</p>
+            <bouton class="flex justify-center mt-7 py-3 w-full">
+            <PlusSquare /> Create catalog
+            </bouton>
         </div>
+        <div class="bg-white border border-gris-foncer p-4 rounded-xl w-full h-auto min-h-56">
+            <div class="flex justify-between items-start">
+            <p class="pt-7 pb-8  text-2xl font-semibold">Christmas 2024</p>
+            <div class="mt-5 opacity-60">
+                <Ellipsis/>
+            </div>
+            </div>
+            <div class="flex gap-2  flex-wrap">
+            <card>Sneakers</card>
+            <card>Boots</card>
+            </div>
+            <p class="text-text py-3 ">Created on 12.27.2024</p>
+            <p class="py-3 font-medium">254 products</p>
         </div>
-        <div class="flex gap-2  flex-wrap">
-        <card>Sneakers</card>
-        <card>Boots</card>
-        </div>
-        <p class="text-text py-3 ">Created on 12.27.2024</p>
-        <p class="py-3 font-medium">254 products</p>
-    </div>
 
-    <div class="bg-white border border-gris-foncer p-4 rounded-xl w-full h-auto min-h-56">
-        <div class="flex justify-between items-start">
-        <p class="py-3  text-2xl font-semibold ">
-            A very big title for a catalog with many thing
-        </p>
-        <div class="mt-5 opacity-60">
-            <Ellipsis/>
+        <div class="bg-white border border-gris-foncer p-4 rounded-xl w-full h-auto min-h-56">
+            <div class="flex justify-between items-start">
+            <p class="py-3  text-2xl font-semibold ">
+                A very big title for a catalog with many thing
+            </p>
+            <div class="mt-5 opacity-60">
+                <Ellipsis/>
+            </div>
+            </div>
+            <div class="flex gap-2 flex-wrap">
+            <card>Sneakers</card>
+            <card>Boots</card>
+            <card>Belt</card>
+            <card>Socks</card>
+            <card>+3</card>
+            </div>
+            <p class="text-text py-3 ">Created on 12.27.2024</p>
+            <p class="py-3  font-medium">254 products</p>
         </div>
-        </div>
-        <div class="flex gap-2 flex-wrap">
-        <card>Sneakers</card>
-        <card>Boots</card>
-        <card>Belt</card>
-        <card>Socks</card>
-        <card>+3</card>
-        </div>
-        <p class="text-text py-3 ">Created on 12.27.2024</p>
-        <p class="py-3  font-medium">254 products</p>
-    </div>
     </div>
 </template>
--- cours-tailwind/app/components/header.vue
+++ cours-tailwind/app/components/header.vue
@@ -3,7 +3,7 @@ import { PlusSquare } from 'lucide-vue-next';
 </script>
 
 <template>
-  <div class="border-b border-gris-foncer p-4 flex sm:flex-row justify-between sm:items-center m-2">
+  <div class="border-b border-gris-foncer p-4 flex sm:flex-row justify-between sm:items-center">
     <p class="font-semibold text-2xl text-center ">My Catalogs</p>
     <bouton class="flex justify-center  w-1/2 sm:w-auto">
       <PlusSquare /> Create catalog
